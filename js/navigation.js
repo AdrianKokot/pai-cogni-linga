f1b23b49ff0a74551210aae0161d02b10ea66a73
Adrian Kokot <splateusz@gmail.com>
2019-11-22 23:05:04 +0100

    poprawka js

--- js/navigation.js
+++ js/navigation.js
@@ -2,19 +2,23 @@ const accountPanel = document.querySelector("#account-panel") || null;
 const mainSection = document.querySelector("body > section") || null;
 const navpanel = document.querySelector("#nav-panel") || null;
 const asideNav = document.querySelector("#aside-nav") || null;
+let bNavpanelActive = false, bAccountpanelActive=false;
 
 if(accountPanel)
 accountPanel.addEventListener("click", () => {
   if (accountPanel.classList.contains("active")) {
     accountPanel.classList.remove("active");
+    bAccountpanelActive=false;
   } else {
     accountPanel.classList.add("active");
+    bAccountpanelActive=true;
   }
 });
 
 if(navpanel){
   navpanel.addEventListener("click", () => {
     asideNav.classList.add("visible");
+    bNavpanelActive = true;
   });
   document.addEventListener('touchstart', handleTouchStart, false);        
   document.addEventListener('touchmove', handleTouchMove, false);
@@ -39,6 +43,7 @@ if(navpanel){
 
       if ( xDiff < 0 ) {
         asideNav.classList.add("visible");
+        bNavpanelActive = true;
       }                       
       
       xDown = null;                                           
@@ -46,10 +51,18 @@ if(navpanel){
 }
   
 
-if(navpanel && mainSection)
+if(mainSection && (navpanel || accountPanel))
 mainSection.addEventListener("click", e => {
-  e.preventDefault();
-  e.stopPropagation();
-  accountPanel.classList.remove("active");
-  asideNav.classList.remove("visible");
+  if(bNavpanelActive) {
+    e.preventDefault();
+    e.stopPropagation();
+    asideNav.classList.remove("visible");
+    bNavpanelActive = false;
+  }
+  if(bAccountpanelActive) {
+    e.preventDefault();
+    e.stopPropagation();
+    accountPanel.classList.remove("active");
+    bAccountpanelActive=false;
+  }
 });
